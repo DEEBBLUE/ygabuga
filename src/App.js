c77cdffeee61912ce945779387d3610eabb17291
Lifter <Sosite.my_dick@gmail.com>
2024-10-22 15:56:32 -0400

    9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { CaseList } from "./component/Case/CaseList";
 import PageUp from "./component/PageUp/page-up";
-import { useState } from "react";
 
 function App(){
     return(
