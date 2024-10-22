c1c6e38efb99cb7af58daa23c5f9c914eaa06fb6
Lifter <Sosite.my_dick@gmail.com>
2024-10-22 08:28:53 -0400

    4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import pageUp from "./component/PageUp/page-up";
 
 function App(){
     return(
         <div className="App">
-            hello
+            <pageUp/>
         </div>
     );
 }
---
+++ src/component/PageUp/page-up.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./page-up.css";
+import Balance from "./Balance/Balance";""
+
+
+const pageUp = () => {
+    return (
+        <div>
+            <Balance/>      
+        </div>
+    )
+}
+
+export default pageUp
