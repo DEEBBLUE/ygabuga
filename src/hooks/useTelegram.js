285289e34ebd50029f2e0640f38d0c0bc87a877e
Lifter <Sosite.my_dick@gmail.com>
2024-10-23 16:17:12 -0400

    3

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -2,8 +2,7 @@ const tg = window.Telegram.WebApp;
 
 export function useTelegram() {
     const onToggleButton = () => {
-        tg.themeParams.color = "#FFAA00"
-        tg.MainButton.setParams();
+        tg.themeParams.button_color = "#FFAA00"
         if(tg.MainButton.isVisible) {
             tg.MainButton.hide();
         } else {
