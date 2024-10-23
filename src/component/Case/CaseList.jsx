be0fcc9f2696f2c089037678cb6b23f03cee2380
Lifter <Sosite.my_dick@gmail.com>
2024-10-23 16:51:17 -0400

    10

--- src/component/Case/CaseList.jsx
+++ src/component/Case/CaseList.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Case from "./Case";
+import useTelegram from '../../hooks/useTelegram.js'
 
 export const CaseList = () => {
+    const {onToggleButton} = useTelegram();
+    const [active,setActive] = useState(false)
+    function handlerClick(){
+        let doc = document.querySelector('.Case');
+        if (active){
+            doc.style.backgroundColor = 'unset';
+            onToggleButton();
+            setActive(false);
+        }
+        else{
+            doc.style.backgroundColor = 'rgd(255,170,0)';
+            setActive(true);
+            onToggleButton();
+        }
+    } 
     return (
         <div>
-            <Case/>
+            <Case onClick={handlerClick}/>
         </div>
     )
 
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -15,3 +15,5 @@ export function useTelegram() {
         onToggleButton,
     }
 }
+
+export default useTelegram;
