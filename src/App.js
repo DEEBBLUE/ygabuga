82763e4dc7ce66702af654c712a1e45f50a5ab2e
Lifter <Sosite.my_dick@gmail.com>
2024-11-11 15:29:21 -0500

    create test post req initData tg object with axios

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { CaseList } from "./component/Case/CaseList";
 import {useTelegram} from "./hooks/useTelegram";
 import axios from "axios";
 
-const BASE_URL = "http://5.159.100.37:8000";
+const BASE_URL = "https://gildbot.ru/api/v1";
 
 function App(){
     const {_ , tg} = useTelegram();
     const initData = tg.initData;
-    res = axios({
-        method: "post",
-        url: BASE_URL + "/userAuth",
-        data: {
-            "initData": initData
+    axios.post(BASE_URL + "/user/Auth",{
+            data: initData 
         }
-    });
-    console.log(res);
+    );
     return(
         <div className="App">
             <CaseList/>
